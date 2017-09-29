59d5aa2ed0be1015b97db39ebfec1f163118f879
drilldripper <ryo.kt.in@gmail.com>
2017-09-30 02:44:48 +0900

    Add rubby toggle when clicking a icon

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,9 +4,24 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-
+let toggleShow = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, 'goRuby');
-  chrome.tabs.insertCSS(null, {file:'ruby.css'}, null);
+  console.log(tab.id);
+  chrome.tabs.insertCSS(null, {file:'css/ruby.css'}, null);
+  chrome.tabs.sendMessage(tab.id, 'goRuby', status =>{
+    console.log('status', status);
+    
+    if(status){
+      if(toggleShow){
+        chrome.tabs.insertCSS(null, {file:'css/display_none.css'}, null); 
+        toggleShow = !toggleShow;
+        console.log('toggle', toggleShow);
+      }else{
+        chrome.tabs.insertCSS(null, {file:'css/display_inline.css'}, null); 
+        toggleShow = !toggleShow;
+        console.log('toggle', toggleShow);
+      }
+    }
+  });
 });
 
--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -1,39 +1,44 @@
 import ej_dict from './kantan-ej-dictionary'
-
-chrome.extension.onMessage.addListener((request, sender, sendRequest) => {
+let initFlag = true;
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
   if (request == 'goRuby') {
 		chrome.storage.sync.get(null, options => {
 			 console.info(options);
-			 console.log('offff');
-			 var result = document.evaluate("/html/body/descendant::text()[name(..)!='script'][name(..)!='style']",
-			  document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-			 for ( var i=0 ; i < result.snapshotLength; i++ ){
-				 var node=result.snapshotItem(i)
-				 if(/[a-zA-Z]/.test(node.textContent)){ //Word Only
-					 var parentNode = node.parentNode;
-					 var span = document.createElement('span');
-					 var nbsp = String.fromCharCode( 160 ); //Remove &nbsp
-					 var words=node.textContent.replace(nbsp,'').split(' ');
-					 for (let word of words) {
-						 var ruby = document.createElement('ruby');
-						 var rb = document.createElement('rb');
-						 rb.innerText=word;
-						 var w = word.replace(/[.;,"?:]+/,'');
-						 var rt = document.createElement('rt');
-						 const hit_word = ej_dict[word];	
-						 if (typeof hit_word === 'undefined'){
-							 // console.log('UNKNOWN');
-						 }else if (hit_word['svl_level'] >= options['level']) {
-							 rt.innerText = hit_word['ja'][0];	
-							 console.log('HIT');
-						 }
-						 ruby.appendChild(rb);
-						 ruby.appendChild(rt);
-						 span.appendChild(ruby);
-				 }
-						 parentNode.replaceChild(span, node);
-				 }
+			 console.log(initFlag);
+			 sendResponse({'state': initFlag});
+			 if(initFlag){
+					let result = document.evaluate("/html/body/descendant::text()[name(..)!='script'][name(..)!='style']",
+					document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+				for ( let i=0 ; i < result.snapshotLength; i++ ){
+					let node=result.snapshotItem(i)
+					if(/[a-zA-Z]/.test(node.textContent)){ //Word Only
+						let parentNode = node.parentNode;
+						let span = document.createElement('span');
+						let nbsp = String.fromCharCode( 160 ); //Remove &nbsp
+						let words=node.textContent.replace(nbsp,'').split(' ');
+						for (let word of words) {
+							let ruby = document.createElement('ruby');
+							let rb = document.createElement('rb');
+							rb.innerText=word;
+							let w = word.replace(/[.;,"?:]+/,'');
+							let rt = document.createElement('rt');
+							const hit_word = ej_dict[word];	
+							if (typeof hit_word === 'undefined'){
+								// console.log('UNKNOWN');
+							}else if (hit_word['svl_level'] >= options['level']) {
+								rt.innerText = hit_word['ja'][0];	
+								console.log('HIT');
+							}
+							ruby.appendChild(rb);
+							ruby.appendChild(rt);
+							span.appendChild(ruby);
+					}
+							parentNode.replaceChild(span, node);
+					}
+				}
+				initFlag = !initFlag;
 			 }
 		});
-  }
+	}
+	return true;
 })
