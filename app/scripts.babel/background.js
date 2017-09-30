e7651f164a881d160729cb7311d5ae8e4dde0b34
drilldripper <ryo.kt.in@gmail.com>
2017-09-30 13:00:07 +0900

    Remove useless process

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,13 +1,9 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.insertCSS(null, {file:'scripts/css/ruby.css'}, null);
   chrome.tabs.sendMessage(tab.id, 'goRuby', status =>{
-    console.log('status', status);
+    // console.log('status', status);
       if(status['display']){
         chrome.tabs.insertCSS(null, {file:'scripts/css/display_none.css'}, null); 
       }else{
--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -4,10 +4,9 @@ let displayFlag = true;
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
 	if (request == 'goRuby') {
 		chrome.storage.sync.get(null, options => {
-			console.log('length', Object.keys(options).length);
 			if( Object.keys(options).length  == 0) {options['level'] = 4;} //Default SVL
-			console.info(options);
-			console.log(initFlag);
+			// console.info(options);
+			// console.log(initFlag);
 			if(initFlag){
 				let result = document.evaluate("/html/body/descendant::text()[name(..)!='script'][name(..)!='style']",
 				document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -22,14 +21,11 @@ chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
 							let ruby = document.createElement('ruby');
 							let rb = document.createElement('rb');
 							rb.innerText=word;
-							let w = word.replace(/[.;,"?:]+/,'');
 							let rt = document.createElement('rt');
 							const hit_word = ej_dict[word];	
 							if (typeof hit_word === 'undefined'){
-								// console.log('UNKNOWN');
 							}else if (hit_word['svl_level'] >= options['level']) {
 								rt.innerText = hit_word['ja'][0];	
-								console.log('HIT');
 							}
 							ruby.appendChild(rb);
 							ruby.appendChild(rt);
